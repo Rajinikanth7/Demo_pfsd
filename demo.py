020ce5cf5ec262182d7dd1f38d5c607d3fbb61bf
Rajinikanth <chintarajinikanth@gmail.com>
2021-02-03 14:44:01 +0530

    changed to 8

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import random as r
 
 def otpgen():
     otp=""
-    for i in range(4):
+    for i in range(8):
         otp+=str(r.randint(1,9))
     print ("Your One Time Password is ")
     print (otp)
